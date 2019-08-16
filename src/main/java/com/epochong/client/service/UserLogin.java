6d20fca5250292c3c66b0d39b1831ea9e392e8e7
epochong <Valentineone@example.com>
2019-08-16 19:52:19 +0800

    Java Swing 实现的聊天室

---
+++ src/main/java/com/epochong/client/service/UserLogin.java
@@ -0,0 +1,17 @@
+package com.epochong.client.service;
+
+import javax.swing.*;
+
+/**
+ * @author epochong
+ * @date 2019/8/16 11:48
+ * @email epochong@163.com
+ * @blog epochong.github.io
+ * @describe
+ */
+public class UserLogin {
+    private JPanel loginPannel;
+    private JPanel labelPanel;
+    private JPanel username;
+    private JTextField textField1;
+}
---
+++ src/main/java/com/epochong/client/service/UserReg.java
@@ -0,0 +1,11 @@
+package com.epochong.client.service;
+
+/**
+ * @author epochong
+ * @date 2019/8/16 11:48
+ * @email epochong@163.com
+ * @blog epochong.github.io
+ * @describe
+ */
+public class UserReg {
+}
